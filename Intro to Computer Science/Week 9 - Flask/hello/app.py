25e0312c9067e681e014d358776d5c1c80a0eda1
Mattheus Stearns <mattheuspstearns@gmail.com>
2025-05-10 17:02:18 +0100

    finish lecture + notes

--- Intro to Computer Science/Week 9 - Flask/hello/app.py
+++ Intro to Computer Science/Week 9 - Flask/hello/app.py
@@ -3,9 +3,12 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        return render_template("greet.html", name=request.form.get("name"))
+    else:
+        return render_template("index.html")
 
 
 #if "name" in request.args:
@@ -14,8 +17,3 @@ def index():
 #    name = "world"
 #This is a bit clunky, so we use something else instead VVV
 #name = request.args.get("name", "world")
-
-@app.route("/greet")
-def greet():
-    name = request.args.get("name", "world")
-    return render_template("greet.html", name=name)
