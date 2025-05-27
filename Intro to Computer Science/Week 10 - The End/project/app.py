f9ac29c111e4e4cbed0f1b7705bec5c3736ed62c
Mattheus Stearns <mattheuspstearns@gmail.com>
2025-05-27 11:21:23 +0200

    week 10 lecture, and creating the initial files

---
+++ Intro to Computer Science/Week 10 - The End/project/app.py
@@ -0,0 +1,26 @@
+import os
+
+# Importing key Libraries
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from datetime import datetime, timezone
+
+# Configure application
+app = Flask(__name__)
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route("/")
+def index():
+    return redirect("/")
