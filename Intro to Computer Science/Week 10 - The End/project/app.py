e5bae29bdbd7d2771845592af06c13113c38491b
Mattheus Stearns <mattheuspstearns@gmail.com>
2025-05-29 21:02:32 +0200

    implemented faulty google sign in

--- Intro to Computer Science/Week 10 - The End/project/app.py
+++ Intro to Computer Science/Week 10 - The End/project/app.py
@@ -1,12 +1,16 @@
 import os
 
 # Importing key Libraries
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, flash, redirect, render_template, request, session, url_for
+from functools import wraps
 from flask_session import Session
 from datetime import datetime, timezone
+from authlib.integrations.flask_client import OAuth
+from dotenv import load_dotenv
 
 # Configure application
 app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
 
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
@@ -21,6 +25,50 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
+# Configure OAuth
+oauth = OAuth(app)
+google = oauth.register(
+    name='google',
+    client_id=os.getenv("GOOGLE_CLIENT_ID"),
+    client_secret=os.getenv("GOOGLE_CLIENT_SECRET"),
+    access_token_url='https://oauth2.googleapis.com/token',
+    access_token_params=None,
+    authorize_url='https://accounts.google.com/o/oauth2/v2/auth',
+    authorize_params={'prompt': 'consent', 'access_type': 'offline'},
+    api_base_url='https://www.googleapis.com/oauth2/v2/',
+    userinfo_endpoint='https://www.googleapis.com/oauth2/v2/userinfo',  # Needed for fetching user info
+    client_kwargs={'scope': 'openid email profile'},
+)
+
+# Decorate routes to require login.
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'user' not in session:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
 @app.route("/")
+@login_required
 def index():
-    return redirect("/")
+    user = session.get('user')
+    return render_template("index.html", user=user)
+
+@app.route("/login")
+def login():
+    redirect_uri = url_for('auth_callback', _external=True)
+    return google.authorize_redirect(redirect_uri)
+
+@app.route('/auth/callback')
+def auth_callback():
+    token = google.authorize_access_token()
+    resp = google.get('userinfo')
+    user_info = resp.json()
+    session['user'] = user_info
+    return redirect('/')
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect('/')
